e17e9709150b4c6424d08329fe4d292602ab3fc6
taiatulislam <taiatulislamapon@gmail.com>
2023-12-22 15:58:27 +0600

    initial file created with mongodb connection and env file

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qp2qmz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+        // await client.connect();
+
+        const contests = client.db("organizerDB").collection("users");
+
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
